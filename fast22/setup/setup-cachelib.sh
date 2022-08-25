acd6a96243750d8668687edb8606b167a3bd88a3
pbhandar <pbhandar@localhost>
2022-08-25 04:09:37 -0500

    setup update

--- fast22/setup/setup-cachelib.sh
+++ fast22/setup/setup-cachelib.sh
@@ -1,12 +1,3 @@
-# {{{ Bash settings
-# abort on nonzero exitstatus
-set -o errexit
-# abort on unbound variable
-set -o nounset
-# don't hide errors within pipes
-set -o pipefail
-# }}}
-
 sudo apt-get update 
 sudo apt install libaio-dev 
 cd ${HOME}
