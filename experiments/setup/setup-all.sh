5a82667f6bff085255e771f932d3be5587cf4326
pbhandar <pbhandar@localhost>
2022-11-24 22:33:28 -0600

    update setup script to incorporate different home dir

--- experiments/setup/setup-all.sh
+++ experiments/setup/setup-all.sh
@@ -15,12 +15,12 @@ backing_dev_path=${1}
 nvm_dev_path=${2}
 backing_file_size_gb=${3}
 nvm_file_size_gb=${4}
-home_dir="${5:-$HOMEDIR}"
 
 ./setup-mount.sh ${backing_dev_path} ${nvm_dev_path} ${backing_file_size_gb} ${nvm_file_size_gb} ${home_dir}
 
 aws_access_key=${5}
 aws_secret=${6}
+home_dir="${7:-$HOMEDIR}"
 
 ./setup-aws.sh ${aws_access_key} ${aws_secret}
 
