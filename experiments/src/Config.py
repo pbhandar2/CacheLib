98861955fc87b584f349b43aacb0d48ecc55ec03
pbhandar <pbhandar@localhost>
2022-12-22 14:49:05 -0600

    update tier sizes for representative traces

--- experiments/src/Config.py
+++ experiments/src/Config.py
@@ -44,10 +44,7 @@ class Config:
         self.block_df = pd.read_csv(self.block_data_path)
         self.run_cachebench_cmd = "../../opt/cachelib/bin/cachebench"
         self.s3_bucket_name = "mtcachedata"
-        self.base_workloads = ["w81", "w47", "w97", \
-                                "w77", "w68", "w78", "w82", \
-                                "w11", "w13", "w54", "w03", \
-                                "w20", "w31"]
+        self.base_workloads = ['w18', 'w14', 'w82', 'w98', 'w54', 'w47', 'w20', 'w11', 'w81', 'w46', 'w32', 'w53', 'w68', 'w36']
 
         # settings used in analysis 
         self.wss_pad_gb = 1
